82131b557a811786c4a9ab4e6c79718a046c110d
nonSerp <fredfarwenel2010@gmail.com>
2025-08-08 17:38:29 +0000

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fredastro.netlify.app/',
-  integrations: [preact()]
+  integrations: [preact(), sitemap()]
 });
